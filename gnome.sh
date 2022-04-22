fb7c7097e53874b0c2d517296f39c201efdfd446
Abdullah AL-Wajih <68817627+abdullahAlwajih@users.noreply.github.com>
2022-04-22 21:28:31 +0100

    Add files via upload

---
+++ gnome.sh
@@ -0,0 +1,108 @@
+# @AlWajihAbdullah
+
+
+gsettings set org.gnome.desktop.app-folders folder-children "['accessories', 'chrome-apps', 'games', 'graphics', 'internet', 'office', 'programming', 'science', 'sound---video', 'system-tools', 'universal-access', 'wine', 'info', 'vuln', 'web', 'db', 'passwd', 'wireless', 're', 'exploit', 'setools', 'sniff', 'post', 'forensics', 'reporting', 'services']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ name "Accessories"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ categories "['Utility']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/chrome-apps/ name "Chrome Apps"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/chrome-apps/ categories "['chrome-apps']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ name "Games"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ categories "['Game']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/graphics/ name "Graphics"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/graphics/ categories "['Graphics']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/internet/ name "Internet"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/internet/ categories "['Network', 'WebBrowser', 'Email']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/office/ name "Office"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/office/ categories "['Office']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/programming/ name "Programming"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/programming/ categories "['Development']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/science/ name "Science"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/science/ categories "['Science']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/sound---video/ name "Sound & Video"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/sound---video/ categories "['AudioVideo', 'Audio', 'Video']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system-tools/ name "System Tools"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system-tools/ categories "['System', 'Settings']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/universal-access/ name "Universal Access"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/universal-access/ categories "['Accessibility']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/wine/ name "Wine"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/wine/ categories "['Wine', 'X-Wine', 'Wine-Programs-Accessories']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/info/ name "01-Information"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/info/ categories "['01-info-gathering', '01-01-dns-analysis', '01-02-identify-live-hosts', '01-03-ids-ips-identification', '01-04-network-scanners', '01-07-osint-analysis', '01-08-route-analysis', '01-10-smb-analysis', '01-11-smtp-analysis', '01-12-snmp-analysis', '01-13-ssl-analysis']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/vuln/ categories "['02-vulnerability-analysis', '02-01-cisco-tools', '02-02-fuzzers', '02-03-voip-tools', '02-05-nessus', '02-06-openvas', '02-07-stress-testing']"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/vuln/ name "02-Vulnerability"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/web/ name '03-WebApps'
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/web/ categories "['03-01-cms-identification', '03-04-web-crawlers', '03-05-web-vulnerability-scanners', '03-06-web-application-proxies', '03-webapp-analysis']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/db/ name "04-Database"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/db/ categories "['04-database-assessment']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/passwd/ name "05-Password"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/passwd/ categories "['01-online-attacks', '05-01-online-attacks', '05-02-offline-attacks', '05-04-pass-hash', '05-05-profile', '05-password-attacks']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/wireless/ name "06-Wireless"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/wireless/ categories "['06-01-wireless-tools', '06-02-bluetooth-tools', '06-03-rfid-nfc-tools', '06-04-other-wireless', '06-05-radio-tools', '06-wireless-attacks']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/re/ name "07-Reversing"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/re/ categories "['07-reverseengineer']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/exploit/ name "08-Exploit"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/exploit/ categories "['08-exploitation-tools']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/sniff/ name "09-Sniffing"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/sniff/ categories "['09-01-network-sniffers', '09-02-network-spoofing', '09-sniffing-spoofing']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/post/ name "10-PostExploit"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/post/ categories "['10-01-os-backdoors', '10-02-tunneling', '10-03-web-backdoors', '10-maintaining-access']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/forensics/ name "11-Forensics"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/forensics/ categories "['11-01-network-forensics', '11-03-digital-forensics', '11-04-forensic-analysis-tools', '11-05-forensic-carving-tools', '11-07-forensic-imaging-tools', '11-11-pdf-forensics-tools', '11-forensics', '11-08-forensic-suites']"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/reporting/ categories "['12-reporting']"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/reporting/ name "12-Reporting"
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/setools/ name "13-SETools"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/setools/ categories "['13-social-engineering-tools']"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/services/ name "14-Services"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/services/ categories "['14-01-gpsd', '14-02-httpd', '14-03-mysqld', '14-04-pcscd', '14-06-sshd', '14-07-radius', '14-08-beef-service', '14-10-dradis-service', '14-11-openvas-service', '14-12-xplico-service', '14-services']"
+
+
+
+
+
+
+
+
+
+
